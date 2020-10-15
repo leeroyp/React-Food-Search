97069c2b8e435513ff9d4d78344b2a59c18bcfb8
Leeroy Phili <leeroyphili@gmail.com>
2020-10-14 23:03:30 -0400

    added more dependencies for navigation

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
-import {createStackNavigator, createAppContainer} from 'react-navigation'
+import {createAppContainer} from "react-navigation";
+import {createStackNavigator} from 'react-navigation-stack';
 
-const navigator = createStackNavigator({
+import SearchScreen from "./src/screens/SearchScreen";
 
-});
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+  },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
