c29528290a7840f36f69dce09fe807b0c48acd5c
Leeroy Phili <leeroyphili@gmail.com>
2020-10-15 20:40:02 -0400

    results files were all the results will be rendered

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,8 +1,9 @@
-import React, { useState , useEffect} from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from '../api/yelp'
 import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
@@ -17,6 +18,10 @@ const SearchScreen = () => {
         onTermSubmit= {()=> searchApi (term)} />
       {errorMessage? <Text>{errorMessage} </Text> : null}
       <Text> we have found {results.length} results</Text>
+      <ResultsList title= "Cost Effective"/>
+      <ResultsList title="Bit Pricier"/>
+      <ResultsList title="Big Spender"/>
+
     </View>
   );
 };
