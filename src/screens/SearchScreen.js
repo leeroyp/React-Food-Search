c2bea77ca2a6a35718743f476bc668d525c1fc16
Leeroy Phili <leeroyphili@gmail.com>
2020-10-15 20:09:14 -0400

    extracted api code to a differet fiel under hooks

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,30 +2,12 @@ import React, { useState , useEffect} from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults]= useState([]);
-  const [errorMessage, setErroMessage] = useState('');
-
-  const searchApi = async(searchTerm) =>{
-      try {
-      const response = await yelp.get('search', {
-          params:{
-              limit: 50,
-              term: searchTerm,
-              location: 'Toronto'
-          }
-      });
-      setResults(response.data.businesses)
-    }catch (err){
-       setErroMessage('Something went wrong ')
-    }
-  }; 
-
-useEffect(() =>{ 
-    searchApi('pasta');
-}, [])
+  const[searchApi, results, errorMessage] = useResults ()
+ 
 
   return (
     <View>
